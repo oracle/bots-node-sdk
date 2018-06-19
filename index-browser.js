7d98d0414b4188c866b89227d102ad6580b6f13c
Henrick Yau <henrick.yau@oracle.com>
2018-06-18 23:53:58 -0700

    [browser] forget about other util for now;

--- index-browser.js
+++ index-browser.js
@@ -1,18 +1,13 @@
 "use strict";
 const MessageModel = require('./lib/message/messageModelBrowser').MessageModel;
-const messageModelUtil = require('./util/');
 /**
  * This is the top level entrypoint to this package when run in browser.  Only MessageModel (using joi-browser)
- * and Util are returned.
+ * is returned.
  *
  * ```javascript
  * const {MessageModel} = require('@oracle/bots-node-sdk');
- * const {webhookUtil} = require("@oracle/bots-node-sdk").Util;
  * ```
  */
 module.exports = {
-  MessageModel: MessageModel,
-  Util: {
-    messageModelUtil: messageModelUtil
-  }
+  MessageModel: MessageModel
 };
