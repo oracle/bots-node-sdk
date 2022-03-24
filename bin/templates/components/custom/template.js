736c52566522a98f75125d925476ccd2aa7f7c3b
Steven Davelaar <steven.davelaar@oracle.com>
2022-03-24 22:37:27 +0100

    fixes for v2.6.1 release

--- bin/templates/components/custom/template.js
+++ bin/templates/components/custom/template.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const {CustomComponentContext} = require('@oracle/bots-node-sdk/lib'); // eslint-disable-line no-unused-vars
-
 // You can use your favorite http client package to make REST calls, however, the node fetch API is pre-installed with the bots-node-sdk.
 // Documentation can be found at https://www.npmjs.com/package/node-fetch
 // Un-comment the next line if you want to make REST calls using node-fetch. 
